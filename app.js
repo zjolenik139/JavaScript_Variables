826e93b43b3da393642a1ef764b9caf04f66dc1b
Zachariah Olenik <zjolenik139@gmail.com>
2022-09-14 23:30:14 -0400

    finished

--- app.js
+++ app.js
@@ -3,8 +3,30 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+const firstName = "Zachariah";
+let lastName = "Olenik";
+var age = 26;
 
-// Exercise 2
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
 
-// Exercise 3
+console.log(`${language} was released in ${createdYear}.`)
+
+let product = "T-Shirt";
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log(`${product} in size ${selectedSize} is ${price} with ${stock} in stock.`);
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(`${title} by ${author} is ${pageCount} pages long. Your last bookmark was page ${bookmark}.`);
